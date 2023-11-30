2b06a0c1fa612332f8eefdd358ea2a282f078869
Guilherme Costa <costa.gguilherme02@gmail.com>
2023-11-30 14:14:08 -0300

    feat: definindo relacao

--- app/Models/Bateria.php
+++ app/Models/Bateria.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Bateria extends Model
 {
@@ -16,4 +17,9 @@ class Bateria extends Model
         'surfista_1_id',
         'surfista_2_id',
     ];
+
+    public function bank_accounts(): HasMany
+    {
+        return $this->hasMany(Surfista::class);
+    }
 }
