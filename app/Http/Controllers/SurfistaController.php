ec66748ad410dfb443aef55d4de658ebcf9b8c0f
Guilherme Costa <costa.gguilherme02@gmail.com>
2023-11-30 13:57:45 -0300

    fix: removendo comments

--- app/Http/Controllers/SurfistaController.php
+++ app/Http/Controllers/SurfistaController.php
@@ -29,35 +29,4 @@ class SurfistaController extends Controller
         return SurfistaResource::collection($surfistas);
     }
 
-    // /**
-    //  * Update the specified resource in storage.
-    //  */
-    // public function update(UpdatesurfistaRequest $request, int $id, int $surfistaId)
-    // {
-    //     $surfista = $this->surfistaRepository
-    //         ->where('user_id', $id)
-    //         ->find($surfistaId);
-
-    //     throw_unless($surfista, surfistaNotFoundException::class);
-
-    //     $surfista = $this->surfistaRepository->update($request->all(), $surfistaId);
-
-    //     return new surfistaResource($surfista);
-    // }
-
-    // /**
-    //  * Remove the specified resource from storage.
-    //  */
-    // public function destroy(int $id, int $surfistaId)
-    // {
-    //     $surfista = $this->surfistaRepository
-    //         ->where('user_id', $id)
-    //         ->find($surfistaId);
-
-    //     throw_unless($surfista, surfistaNotFoundException::class);
-
-    //     $this->surfistaRepository->delete($surfistaId);
-
-    //     return response()->json(status: Response::HTTP_NO_CONTENT);
-    // }
 }
